9f882fad6cb862bf2fb5f5045a957bf533fba46c
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2023-09-10 01:15:01 +0530

    Initial commit

---
+++ springcore/src/main/java/com/springcore/Collections/test.java
@@ -0,0 +1,18 @@
+package com.springcore.Collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collections/collectionconfig.xml");
+		Emp emp1=(Emp) context.getBean("emp1");
+		
+		System.out.println(emp1.getName());
+		System.out.println(emp1.getPhones());
+		System.out.println(emp1.getAddress());
+		System.out.println(emp1.getCourses());
+		
+	}
+}
